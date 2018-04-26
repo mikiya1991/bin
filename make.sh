40fd9180f5d98bd77f4da60de5fc75c121537371
anakin_wang <anakin_wang@realsil.com.cn>
2018-04-26 09:45:29 +0800

    change: rm unnecessary scripts & add vimrc

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-make -C ../../.. ffmpeg_build V=1\
-&& make -C ../../.. peacock_build V=1\
-&& make -C ../../.. peacock_romfs \
-&& make -C ../../.. ffmpeg_romfs \
-&& make -C ../../.. image \
-&& make -C ../../.. pack
